41010222e803aae64274b5c7669c9508231180d6
Pragonna <alizadeemil5@gmail.com>
2024-07-12 12:43:12 +0400

    First Commit

---
+++ BasicConfgWithEnv/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+Environment.SetEnvironmentVariable("Pragonna_TestEnv", "Development");
+
+IConfigurationRoot configurationRoot = new ConfigurationBuilder()
+    .AddJsonFile("appSettings.json")
+    .AddEnvironmentVariables(prefix:"Pragonna_")
+    .Build();
+
+var dbName=configurationRoot["DatabaseName"];
+var username = configurationRoot["DefaultUsername"];
+var envName = configurationRoot["TestEnv"];
+
+Console.WriteLine($"Db:  {dbName}\nUsr: {username}\nEnv: {envName}");
+
+Console.ReadLine();
+
---
+++ BasicConfiguration/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+    .AddJsonFile("appSettings.json");
+
+IConfigurationRoot configurationRoot = configurationBuilder.Build();
+
+var dbName = configurationRoot["DatabaseName"];
+var username = configurationRoot["DefaultUsername"];
+
+Console.WriteLine($" Database name is {dbName}\n Username is {username}");
+
+Console.ReadLine();
